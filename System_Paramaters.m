abd1bd0e7210d4739d53c74bee5b480bc2d0d5cb
David Bascelli <davidb996@gmail.com>
2018-11-15 23:07:57 -0500

    Inital creation of system

---
+++ System_Paramaters.m
@@ -0,0 +1,3 @@
+Fa = 10E9; % 10 GHz, the "clock rate" for the analog portions of the simulation. Should be much higher then the switch rate or the sample speed
+
+Fs = 10E3; % 10 kHz, set switch clock rate
